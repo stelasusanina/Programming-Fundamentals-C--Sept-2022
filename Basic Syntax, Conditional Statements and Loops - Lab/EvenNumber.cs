1840dadb1fcbac38028a085c8f44e81418b84857
stelasusanina <ssttteelllaa@gmail.com>
2022-09-24 18:12:09 +0300

    Basic Syntax, Conditional Statements and Loops

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/EvenNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _12.EvenNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            while (input%2 != 0)
+            {
+                input = Math.Abs(input);
+
+                Console.WriteLine("Please write an even number.");
+
+
+                input = int.Parse(Console.ReadLine());
+            }
+
+            if (input%2 == 0)
+            {
+                Console.WriteLine($"The number is: {Math.Abs(input)}");
+            }
+        }
+    }
+}
---
+++ Basic Syntax, Conditional Statements and Loops - Lab/SumofOddNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _09.SumofOddNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int numbers = 1;
+            int sum = 0;
+            while (count > 0)
+            {
+                Console.WriteLine(numbers);
+                
+                sum += numbers;
+                numbers += 2;
+                count--;
+            }
+            Console.WriteLine($"Sum: {sum}");
+
+        }
+    }
+}
